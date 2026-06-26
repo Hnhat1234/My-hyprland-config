0a57db2fac85ba1c8ef83e58175db32b08d9489e
Hnhat1234 <88219086+Hnhat1234@users.noreply.github.com>
2026-06-26 10:48:07 +0700

    Delete widgets directory

--- widgets/clock.tsx
+++
@@ -1,9 +0,0 @@
-import { createPoll } from "ags/time"
-import { produce } from "immer"
-
-export function Clock(){
-    const date = createPoll("", 1000, () => new Date().toString())
-    return (
-        <label class="clock" label={date} />
-    )
-}
